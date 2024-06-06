6514a7875ca8e681b1bd7c87ceb463b5ce12c204
Mattia Gisiger <mattia.gisiger@gmail.com>
2024-06-06 09:02:51 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM golang:latest as builder
+
+WORKDIR /app
+
+COPY . .
+
+RUN go mod download
+
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+
+FROM alpine:latest  
+RUN apk --no-cache add ca-certificates
+
+WORKDIR /root/
+
+COPY --from=builder /app/main .
+COPY --from=builder /app/views ./views
+
+EXPOSE 3000
+
+CMD ["./main"]
--- app.go
+++ app.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{})
+	})
+
+	app.Listen(":3000")
 }
